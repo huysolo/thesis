0bd88bc809f61c376ad6a50cc26f40b0ec2c3512
huysolo <32314642+huysolo@users.noreply.github.com>
2018-03-19 15:50:34 +0700

    Fix Autho Service

--- frontend/src/app/core/auth.service.ts
+++ frontend/src/app/core/auth.service.ts
@@ -3,17 +3,15 @@ import { CurrUserInfo } from '../user/pages/login/curr-user-info';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Injectable()
-export class AuthService{
-  isLogin :boolean;
+export class AuthService {
+  isLogin: boolean;
   username: String;
   isStudent: Boolean;
   token: string;
   constructor() {
-    this.isLogin = (localStorage.getItem('isLogin') == 'true')? true:false;
+    this.isLogin = localStorage.getItem('isLogin') === 'true';
     this.username = localStorage.getItem('username');
     this.token = localStorage.getItem('token');
-    this.isStudent = (localStorage.getItem('isStudent') == 'true')? true: false;
+    this.isStudent = localStorage.getItem('isStudent') === 'true';
   }
-
-
 }
